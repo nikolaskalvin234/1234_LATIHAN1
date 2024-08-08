8e53a6350dbc440317140f7c631bdf3d303dadc1
nikolaskalvin <nikolaskalvin@gmail.com>
2024-08-08 21:44:12 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1234LATIHAN_1_
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            for (int a = 1; a <=5; a++)
+        {
+            for (int b = 1; b <=a; b++)
+{
+        Console.Write(b + "");
+}
+        Console.WriteLine("\n");
+}
+        Console.ReadKey();
+        }
+    }
+  }
